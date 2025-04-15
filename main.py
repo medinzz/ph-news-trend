5a15fbef582246b37fe31f3df3f9fa710cd26f86
Joshua Merwin Medina <60117514+medinzz@users.noreply.github.com>
2025-04-15 09:57:42 +0000

    minor update to use case

--- main.py
+++ main.py
@@ -2,33 +2,11 @@ import pandas as pd
 import json
 
 from datetime import datetime
-from news.crawler import refresh_news_articles, get_article_content
+from news.crawler import refresh_news_articles
 
 ########## Getting news articles ################
 refresh_news_articles(
     start_date=datetime.today().strftime('%Y-%m-%d'), 
     end_date=datetime.today().strftime('%Y-%m-%d'))
-
-
-########## Extracting article content ##############
-# with open('news_articles.json', 'r') as f:
-#     data = json.load(f)
-
-# # Step 2: Flatten the structure
-# records = []
-# for category, dates in data.items():
-#     for date, urls in dates.items():
-#         for url in urls:
-#             records.append({
-#                 'category': category,
-#                 'date': date,
-#                 'url': url
-#             })
-
-# # Step 3: Convert to DataFrame
-# df = pd.DataFrame(records)
-# urls = df['url'].to_list()
-
-# get_article_content(article_links=urls)
 
 
