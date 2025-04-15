f612d708deb83d41e44c3b5e8d3e234da5d3a687
Joshua Merwin Medina <60117514+medinzz@users.noreply.github.com>
2025-04-15 09:57:28 +0000

    addded article content cleaner

--- util/tools.py
+++ util/tools.py
@@ -1,6 +1,10 @@
-# URL TOOLS
-from urllib.parse import urlparse
+import html2text
 
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+
+# URL TOOLS
 def parse_inq_art_url(url) -> dict:
     parsed = urlparse(url)
     # Extract subdomain (e.g., 'pop', 'globalnation', 'business')
@@ -21,12 +25,41 @@ def parse_inq_art_url(url) -> dict:
     }
 
 # MISC 
-from datetime import datetime
+def clean_article(
+        article: str, 
+        unwanted_ids: list = [], 
+        unwanted_classes: list = [], 
+        unwanted_elements: list = ['script', 'style']) -> str:
+    '''
+    Cleans an HTML article by removing unwanted elements, IDs, and classes, 
+    and converts the cleaned HTML into Markdown format.
+    Args:
+        article (str): The HTML content of the article to be cleaned.
+        unwanted_ids (list, optional): A list of element IDs to remove from the HTML. Defaults to an empty list.
+        unwanted_classes (list, optional): A list of CSS class selectors to remove from the HTML. Defaults to an empty list.
+        unwanted_elements (list, optional): A list of HTML tag names to remove from the HTML. Defaults to ['script', 'style'].
+    Returns:
+        str: The cleaned article content converted to Markdown format.
+    '''
+    
+    soup = BeautifulSoup(article, 'html.parser')
 
-def find_date_in_list(items, date_format='%I:%M %p %B %d, %Y'):
-    for item in items:
-        try:
-            return datetime.strptime(item.replace('/', '').strip(), date_format)
-        except ValueError:
-            continue
-    return None
+    # Remove unwanted elements by ID
+    for unwanted_id in unwanted_ids:
+        tag = soup.find(id=unwanted_id)
+        if tag:
+            tag.decompose()
+    
+    for tag in soup(unwanted_elements):
+        tag.decompose()
+
+    # Remove unwantend elements by CLASS
+    for cls in unwanted_classes:
+        for tag in soup.select(cls):
+            tag.decompose()
+
+    # Convert cleaned HTML to Markdown
+    clean_html = str(soup)
+    markdown = html2text.html2text(clean_html, bodywidth = 0)
+
+    return markdown
