46bdf3ea2d60c5773ab78959eab4175e8da31b64
Joshua Merwin Medina <60117514+medinzz@users.noreply.github.com>
2025-04-28 08:55:18 +0000

    crawl both inquirer and abscbn

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from datetime import datetime, timedelta
 from news.crawler import refresh_news_articles
-from news.apis.abscbn import get_articles
+from news.apis.abscbn import get_abscbn_articles
 
-start_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 
-articles = get_articles(
-    start_date=start_date
+start_date = (datetime.today() - timedelta(days=3)).strftime('%Y-%m-%d')
+
+################ Getting news articles from abscbn ################
+get_abscbn_articles(start_date=start_date)
+
+################ Getting news articles from inquirer ################
+refresh_news_articles(
+    start_date=start_date,
+    end_date=datetime.today().strftime('%Y-%m-%d')
 )
-
-print(articles)
-
-########## Getting news articles from inquirer ################
-# refresh_news_articles(
-#     start_date=start_date,
-#     end_date=datetime.today().strftime('%Y-%m-%d')
-# )
