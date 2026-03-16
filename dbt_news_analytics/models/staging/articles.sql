54e0eef08dddb05765e4c3729d0c9181ab1efd5b
Joshua Merwin V. Medina <medinajoshuamerwin@gmail.com>
2026-03-16 13:44:11 +0800

    update staging sql

--- dbt_news_analytics/models/staging/articles.sql
+++ dbt_news_analytics/models/staging/articles.sql
@@ -1,20 +1,11 @@
-WITH
-    source_data AS (
-        SELECT
-            * EXCEPT (tags),
-            SPLIT(LOWER(REPLACE(tags, '-', ' ')), ',') AS tags
-        FROM {{ source('ph_news_raw', 'articles_raw') }}
-    )
-SELECT
-    DISTINCT
-    id,
-    source,
-    url,
-    title,
-    category,
-    author,
-    date,
-    publish_time,
-    content,
-    tags,
-FROM source_data
+-- To Clean the data from new lines and remove "Read Next" in content as it points to a different article.
+
+UPDATE articles_raw
+SET 
+    -- 1. Split by 'Read Next' and take the first part
+    -- 2. Replace double newlines with a single space (or '')
+    -- 3. Trim the result
+    content = TRIM(REPLACE(SPLIT_PART(content, 'Read Next', 1), E'\n\n', ' ')),
+    title = TRIM(REPLACE(title, E'\n\n', ' '))
+WHERE source = 'inquirer'
+AND content LIKE '%Read Next%';
